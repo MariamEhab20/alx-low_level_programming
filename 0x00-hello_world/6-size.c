d5b08b71cfdc682fca5f4606538f0e7ec634b053
MariamEhab20 <me@example.com>
2023-07-13 17:21:00 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Enty point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(S)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(S)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(S)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(S)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(S)\n", sizeof(afloat));
+   return (0);
+}
