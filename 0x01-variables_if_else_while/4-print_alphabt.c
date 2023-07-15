ded904c1ba41485b297927ad86bd17285f0f4e08
MariamEhab20 <me@example.com>
2023-07-14 17:20:37 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,10 +4,11 @@
  * Description: point all aplhabet letters expect q and e
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char ch = 'a';
 	while (ch <= 'z')
+
 	{
 		if (ch == 'e' || ch == 'q')
 			ch++;
