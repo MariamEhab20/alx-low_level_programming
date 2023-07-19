2fd4badbd1d9ebfbf05754d72860ebd2e762db30
MariamEhab20 <me@example.com>
2023-07-18 18:58:19 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,10 +27,10 @@ int prod, mult, num;
 					if (prod <= 99)
 
 					_putchar (' ');
-					if (prod >= 100)
+	if (prod >= 100)
 			{
-				_putchar ((prod / 100) + 48); 
-				_putchar ( (prod / 10) % 10 + 48);
+				_putchar ((prod / 100) + 48);
+				_putchar ((prod / 10) % 10 + 48);
 			} else if (prod <= 99 && prod >= 10)
 				_putchar((prod / 10) + 48);
 			_putchar ((prod % 10) + 48);
