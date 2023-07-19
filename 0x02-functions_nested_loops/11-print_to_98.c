350d1487c69ddca7c7bfe05e8412572a434b68ca
MariamEhab20 <me@example.com>
2023-07-18 18:29:18 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98
+ * print_to_98 - print n to 98 counts separated
+ *   by space and number should be printed in order
  * @n: input
  */
 void print_to_98(int n)
