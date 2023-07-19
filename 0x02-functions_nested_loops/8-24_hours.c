0fba9c56b21c2dd6af11be6052194f681e42bcf3
MariamEhab20 <me@example.com>
2023-07-18 17:40:49 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,13 @@ void jack_bauer(void)
 {
 	int min, hr;
 
-	for(hr =0; hr <= 23; h++)
+	for(hr = 0; hr <= 23; h++)
 	{
 		for(min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + 48); 
 			_putchar((hr % 10) + 48);
-			_putchar(":");
+			_putchar(':');
 			_putchar((min / 10) + 48);
 			_putchar((min % 10) + 48);
 			_putchar('\n');
