3fc4c6ac3bc71fcbf59a20a3b7ef01fedec6539c
MariamEhab20 <me@example.com>
2023-07-20 16:09:59 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite 1oop
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop inconing : (\n");
+
+	i = 0;
+
+	/*while (1 < 10)*/
+	/*{*/
+	/* putchar (i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
