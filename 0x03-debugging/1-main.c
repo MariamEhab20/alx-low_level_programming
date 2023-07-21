134afe0195d87db8b7deae981cb180150ac866f7
MariamEhab20 <me@example.com>
2023-07-20 17:26:54 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Comment out (don’t delete it!) the part of the code
- * that is causing the output to go into an infinite loop
+ * main - Comment outthe part of the code that is causing the output to go into an infinite loop
  * Return: 0
  */
 
